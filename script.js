f035efe510a0c1c43bad1f12259111c7d9b6d89e
Anna <anna.bay@list.ru>
2020-08-05 00:11:40 +0400

    secont commit

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ const palindrom = (str) =>{
 }
 console.log(palindrom("qaxaq"))
 console.log(palindrom("hamakarg"))
+
+/////////////////////////////////////////
+const bigDigit = (number) =>{
+	let digit = number.split("")
+	let max = digit[0]
+	for (let i = 0; i < digit.length; i++) {
+		if(digit[i]>max){
+			max = digit[i]
+		}
+	}
+	
+	return max
+}
+
+console.log(bigDigit("37"))
+
+//////////////////////////////////////////////////////////////
+
+const words = (string) =>{
+	let splitByWord = string.split(" ")
+	return splitByWord
+}
+
+console.log(words("I love JavaScript so much"))
+
+//////////////////////////////////////////////////
+// 1. Write a JavaScript program to split values of two given arrays into two groups. 
+// If an element in filter is truthy, the corresponding element in the collection 
+// belongs to the first group; otherwise, it belongs to the second group.
+
+// This task was not clear for me
